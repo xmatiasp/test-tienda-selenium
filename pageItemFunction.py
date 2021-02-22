4a8fe2a5154f390088bd1e3b2f3ca6aa56ce7fec
matt <xmatiasp@gmail.com>
2021-02-22 18:23:04 -0300

    Agregando By y waits

--- pageItemFunction.py
+++ pageItemFunction.py
@@ -1,15 +1,24 @@
+from selenium.webdriver.common.by import By
+
+
 class PageItemsFunction:
+
     def __init__(self, mi_driver):
         self.driver = mi_driver
-        self.input_box = 'quantity_wanted'
+        self.input_box = (By.ID, 'quantity_wanted')
+        self.orange_color = (By.ID, 'color_1')
 
     def hacer_click_by_xpath(self, elemento_by_xpath,cantidad_clicks):
         for clicks in range(cantidad_clicks):
             self.driver.find_element_by_xpath(elemento_by_xpath).click()
 
     def introducir_cantidad(self, cantidad):
-        self.driver.find_element_by_id(self.input_box).clear()
-        self.driver.find_element_by_id(self.input_box).send_keys(cantidad)
+        self.driver.find_element(*self.input_box).clear()
+        self.driver.find_element(*self.input_box).send_keys(cantidad)
+
+    def color_change(self):
+        self.driver.find_element(*self.orange_color).click()
+
 
     def obtener_numero_Input(self):
         return self.driver.find_element_by_id("quantity_wanted").get_attribute("value")
--- searchItem.py
+++ searchItem.py
@@ -1,6 +1,5 @@
 import unittest
 from selenium import webdriver
-import time
 from pageIndex import PageIndex
 from pageItem import PageItems
 from pageItemFunction import PageItemsFunction
@@ -16,27 +15,23 @@ class SearchCases(unittest.TestCase):
 
     def test_search_no_elements(self):
         self.indexPage.search('hola')
-        time.sleep(2)
         self.assertEqual(self.itemsPage.return_no_element_text(), 'No results were found for your search "hola"')
 
     def test_search_find_dresses(self):
 
         self.indexPage.search('dress')
-        time.sleep(2)
         self.assertTrue('DRESS' in self.itemsPage.return_section_title())
 
     def test_search_find_tshirt(self):
 
         self.indexPage.search('t-shirt')
-        time.sleep(2)
         self.assertTrue('T-SHIRT' in self.driver.find_element_by_xpath('//*[@id="center_column"]/h1/span[1]').text,self.driver.find_element_by_xpath('//*[@id="center_column"]/h1/span[1]').text)
 
     def test_carrito_tshirts(self):
         self.driver.find_element_by_xpath('//*[@id="block_top_menu"]/ul/li[3]/a').click()
-        self.driver.find_element_by_id('color_1').click()
+        self.functionItemsPage.color_change()
         self.functionItemsPage.introducir_cantidad('25')
         self.functionItemsPage.hacer_click_by_xpath('//*[@id="quantity_wanted_p"]/a[2]/span/i', 3)
-        time.sleep(3)
         self.assertEqual(self.functionItemsPage.obtener_numero_Input(), '28')
 
     def tearDown(self):
