dc5c0ee445a0b87be2800cf420a89570a4364aa1
matt <xmatiasp@gmail.com>
2021-02-22 13:44:26 -0300

    actualizado page object

---
+++ pageItemFunction.py
@@ -0,0 +1,12 @@
+class PageItemsFunction:
+    def __init__(self, mi_driver):
+        self.driver = mi_driver
+        self.input_box = 'quantity_wanted'
+
+    def hacer_click_by_xpath(self, elemento_by_xpath,cantidad_clicks):
+        for clicks in range(cantidad_clicks):
+            self.driver.find_element_by_xpath(elemento_by_xpath).click()
+
+    def introducir_cantidad(self, cantidad):
+        self.driver.find_element_by_id(self.input_box).clear()
+        self.driver.find_element_by_id(self.input_box).send_keys(cantidad)
--- searchItem.py
+++ searchItem.py
@@ -1,34 +1,44 @@
 import unittest
 from selenium import webdriver
 import time
+from pageIndex import PageIndex
+from pageItem import PageItems
+from pageItemFunction import PageItemsFunction
 
 class SearchCases(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome('chromedriver.exe')
         self.driver.get('http://automationpractice.com/index.php')
+        self.indexPage = PageIndex(self.driver)
+        self.itemsPage = PageItems(self.driver)
+        self.functionItemsPage = PageItemsFunction(self.driver)
 
     def test_search_no_elements(self):
-
-        self.driver.find_element_by_id('search_query_top').send_keys('hola')
-        self.driver.find_element_by_name('submit_search').click()
+        self.indexPage.search('hola')
         time.sleep(2)
-        self.assertEqual(self.driver.find_element_by_xpath('//*[@id="center_column"]/p').text, 'No results were found for your search "hola"')
+        self.assertEqual(self.itemsPage.return_no_element_text(), 'No results were found for your search "hola"')
 
     def test_search_find_dresses(self):
 
-        self.driver.find_element_by_id('search_query_top').send_keys('dress')
-        self.driver.find_element_by_name('submit_search').click()
+        self.indexPage.search('dress')
         time.sleep(2)
-        self.assertTrue('DRESS' in self.driver.find_element_by_xpath('//*[@id="center_column"]/h1/span[1]').text)
-        #probando
+        self.assertTrue('DRESS' in self.itemsPage.return_section_title())
 
     def test_search_find_tshirt(self):
 
-        self.driver.find_element_by_id('search_query_top').send_keys('t-shirt')
-        self.driver.find_element_by_name('submit_search').click()
+        self.indexPage.search('t-shirt')
         time.sleep(2)
         self.assertTrue('T-SHIRT' in self.driver.find_element_by_xpath('//*[@id="center_column"]/h1/span[1]').text,self.driver.find_element_by_xpath('//*[@id="center_column"]/h1/span[1]').text)
+
+    def test_carrito_tshirts(self):
+        self.driver.find_element_by_xpath('//*[@id="block_top_menu"]/ul/li[3]/a').click()
+        self.driver.find_element_by_id('color_1').click()
+        self.functionItemsPage.introducir_cantidad('25')
+        self.functionItemsPage.hacer_click_by_xpath('//*[@id="quantity_wanted_p"]/a[2]/span/i', 3)
+        time.sleep(3)
+        self.assertEqual(self.driver.find_element_by_id("quantity_wanted").get_attribute("value"), '28')
+
 
     def tearDown(self):
         self.driver.close()
